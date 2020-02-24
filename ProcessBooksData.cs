fe40424c5f7a461f41762ce2d120ffc6fc3d9fe8
Kiran <41660600+kiran1dhaliwal@users.noreply.github.com>
2020-02-25 08:00:45 +1300

    Add files via upload

---
+++ ProcessBooksData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    class ProcessBooksData
+    {
+        
+        Queue<Book> allBooks = new Queue<Book>();
+        Queue<Book> ComputerScienceBooks = new Queue<Book>();
+        Queue<Book> NetworkingBooks = new Queue<Book>();
+        Queue<Book> MathematicsBooks = new Queue<Book>();
+        Queue<Book> SoftwareDevelopementBooks = new Queue<Book>();
+        Book book = new Book();
+
+        //getting computer science books
+        public Queue<Book> getComputerSienceBooks(List<Book> books)
+        {
+            foreach (Book book in books)
+            {
+                if (book.Category == "Computer Science")
+                {
+                    ComputerScienceBooks.Enqueue(book);
+                }
+            }
+            
+            return ComputerScienceBooks;
+        }
+
+        //getting networking books
+        public Queue<Book> getNetworkingBooks(List<Book> books)
+        {
+            foreach (Book book in books)
+            {
+                if (book.Category == "Networking")
+                {
+                    NetworkingBooks.Enqueue(book);
+                }
+            }
+            
+            return NetworkingBooks;
+        }
+
+        //getting Mathematics books
+        public Queue<Book> getMathematicsBooks(List<Book> books)
+        {
+            foreach(Book book in books)
+            {
+                if (book.Category == "Mathematics")
+                {
+                    MathematicsBooks.Enqueue(book);
+                }
+            }   
+            
+            return MathematicsBooks;
+        }
+
+        //getting software developement books
+        public Queue<Book> getSofwareDevelopementBooks(List<Book> books)
+        {
+            foreach (Book book in books)
+            {
+                if (book.Category == "Software Development")
+                {
+                    SoftwareDevelopementBooks.Enqueue(book);
+                }
+            }
+
+            return SoftwareDevelopementBooks;
+        }
+        
+    }
+}
---
+++ ReadDataFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    class ReadDataFile
+    {
+        public List<Book> readBooks(string fileName)
+        {
+            List<Book> unchechedBooks = new List<Book>();
+            StreamReader reader = new StreamReader(fileName);
+            string line = "";
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] words = line.Split(',');
+                int ArrayLength = words.Length;
+                Book book = new Book();
+
+                book.ISBN = int.Parse(words[0]);
+                for (int i = 1; i < (ArrayLength - 4); i++)
+                {
+                    book.AuthorsName = line.Split(':');
+                }
+                book.BookName = words[ArrayLength - 4];
+                book.Publisher = words[ArrayLength - 3];
+                book.ReleasedYear = int.Parse(words[ArrayLength - 2]);
+                book.Category = words[ArrayLength - 1];
+
+                unchechedBooks.Add(book);
+            }
+
+            return unchechedBooks;
+        }
+        
+    }
+}
